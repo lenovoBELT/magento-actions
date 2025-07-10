1fb1bab9fe818d1e22628e1f99b5d79194db6192
Leandro Barboza <leandro.santos@beltnutrition.com.br>
2025-07-10 13:26:01 -0300

    Removes docker download on multiple php versions

--- config/utils/common-magento-installer.sh
+++ config/utils/common-magento-installer.sh
@@ -22,6 +22,7 @@ then
 else
   if [ "$INPUT_OPENSEARCH" = "1" ]
   then
+    bin/magento setup:upgrade
     echo 'teste2';
    ## bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation --search-engine="opensearch" --opensearch-host="$SEARCHENGINE" --opensearch-port=9200 --disable-modules="$INPUT_DISABLE_MODULES"
   fi
