e6f79d761c556170312d98ecbf4b171723c36f8e
Leandro Barboza <leandro.santos@beltnutrition.com.br>
2025-07-10 15:27:42 -0300

    Removes docker download on multiple php versions

--- config/utils/common-magento-installer.sh
+++ config/utils/common-magento-installer.sh
@@ -25,7 +25,7 @@ else
     bin/magento deploy:mode:set developer
     bin/magento setup:upgrade
     bin/magento setup:di:compile
-    bin/magento setup:static-content:deploy en_US pt_BR -j4 -f
+    bin/magento setup:static-content:deploy en_US pt_BR --theme BeltNutrition/default --theme BeltNutrition/prescritores --theme BeltNutrition/kids -f
     composer dump-autoload -o
    ## bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation --search-engine="opensearch" --opensearch-host="$SEARCHENGINE" --opensearch-port=9200 --disable-modules="$INPUT_DISABLE_MODULES"
   fi
