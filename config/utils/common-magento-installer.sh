861601b1728034ba70f4bdbf08bcd8ab78501367
Leandro Barboza <leandro.santos@beltnutrition.com.br>
2025-07-10 10:08:50 -0300

    Removes docker download on multiple php versions

--- config/utils/common-magento-installer.sh
+++ config/utils/common-magento-installer.sh
@@ -18,7 +18,7 @@ fi
 
 if [ "$INPUT_ELASTICSUITE" = "1" ]
 then
-  bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation --es-hosts="$SEARCHENGINE:9200" --es-user="" --es-pass="" --disable-modules="$INPUT_DISABLE_MODULES"
+  #bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation --es-hosts="$SEARCHENGINE:9200" --es-user="" --es-pass="" --disable-modules="$INPUT_DISABLE_MODULES"
 else
   if [ "$INPUT_ELASTICSEARCH" = "1" ]
   then
