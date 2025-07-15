2856b4e4d40f592f607e431e14ffe576a4cc58c8
Leandro Barboza <leandro.santos@beltnutrition.com.br>
2025-07-15 08:55:26 -0300

    Removes docker download on multiple php versions

--- config/utils/common-magento-installer.sh
+++ config/utils/common-magento-installer.sh
@@ -21,8 +21,11 @@ fi
   bin/magento deploy:mode:set production
   echo " ========= SETUP:UPGRADE ========="
   bin/magento setup:upgrade
+  echo " ========= END OF UPGRADE ========="
   echo " ========= DI:COMPILE ========="
   bin/magento setup:di:compile
-  echo " ========= STATIC-CONTENT ========="
+  echo " ========= END OF DI ========="
+  echo " ========= STATIC-CONTENT DEPLOYMENT ========="
   bin/magento setup:static-content:deploy pt_BR -f
+  echo " ========= END OF STATIC-CONTENT DEPLOYMENT ========="
  ## bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation --search-engine="opensearch" --opensearch-host="$SEARCHENGINE" --opensearch-port=9200 --disable-modules="$INPUT_DISABLE_MODULES"
